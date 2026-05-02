cf0091637578c934d3da3d8b2a60305238dd9ece
alikhvn <alihanbirmanuli@gmail.com>
2026-05-02 08:39:45 +0500

    fix

--- run.py
+++ run.py
@@ -1,5 +1,23 @@
 import asyncio
+import threading
+from http.server import HTTPServer, BaseHTTPRequestHandler
 from app.main import main
 
-if __name__ == "__main__":
+class Handler(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.end_headers()
+        self.wfile.write(b"OK")
+
+def run_server():
+    port = 10000  
+    server = HTTPServer(("", port), Handler)
+    server.serve_forever()
+
+def run_async():
     asyncio.run(main())
+
+if __name__ == "__main__":
+    threading.Thread(target=run_server).start()
+
+    run_async()
